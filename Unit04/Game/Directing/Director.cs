d8d373b6a9c63c64fbd3072127a92900ed66ab29
David Uchytil <davidu17@byui.edu>
2022-11-25 15:03:00 -0700

    Finished Version

---
+++ Unit04/Game/Directing/Director.cs
@@ -0,0 +1,118 @@
+using System.Collections.Generic;
+using Unit04.Game.Casting;
+using Unit04.Game.Services;
+
+
+namespace Unit04.Game.Directing
+{
+    /// <summary>
+    /// <para>A person who directs the game.</para>
+    /// <para>
+    /// The responsibility of a Director is to control the sequence of play.
+    /// </para>
+    /// </summary>
+    public class Director
+    {
+        private KeyboardService _keyboardService = null;
+        private VideoService _videoService = null;
+
+        /// <summary>
+        /// Constructs a new instance of Director using the given KeyboardService and VideoService.
+        /// </summary>
+        /// <param name="keyboardService">The given KeyboardService.</param>
+        /// <param name="videoService">The given VideoService.</param>
+        public Director(KeyboardService keyboardService, VideoService videoService)
+        {
+            this._keyboardService = keyboardService;
+            this._videoService = videoService;
+        }
+
+        /// <summary>
+        /// Starts the game by running the main game loop for the given cast.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        public void StartGame(Cast cast)
+        {
+            _videoService.OpenWindow();
+            while (_videoService.IsWindowOpen())
+            {
+                GetInputs(cast);
+                DoUpdates(cast);
+                DoOutputs(cast);
+            }
+            _videoService.CloseWindow();
+        }
+
+        /// <summary>
+        /// Gets directional input from the keyboard and applies it to the robot.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        private void GetInputs(Cast cast)
+        {
+            Actor robot = cast.GetFirstActor("robot");
+            Point velocity = _keyboardService.GetDirection();
+            robot.SetVelocity(velocity);     
+        }
+
+        /// <summary>
+        /// Updates the robot's position and resolves any collisions with artifacts.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        private void DoUpdates(Cast cast)
+        {
+            Actor banner = cast.GetFirstActor("banner");
+            Actor robot = cast.GetFirstActor("robot");
+            List<Actor> rocks = cast.GetActors("rocks");
+            List<Actor> gems = cast.GetActors("gems");
+
+            banner.SetText("Score: " + robot.GetScore().ToString());
+            int maxX = _videoService.GetWidth();
+            int maxY = _videoService.GetHeight();
+            robot.MoveNext(maxX, maxY);
+
+
+            //SET THIS FOR ROCK CONDITION
+            foreach (Actor actor in rocks)
+            {
+                //Making artifacts move
+                Artifact artifact = (Artifact) actor;
+                artifact.MoveNext(maxX, maxY);
+
+                //SET CONDITION HERE
+                if (robot.GetPosition().Equals(actor.GetPosition()))
+                {
+                    robot.SetScore(-1);
+                    banner.SetText("Score: " + robot.GetScore().ToString());
+                }
+            } 
+
+            //SET THIS FOR GEM CONDITION
+            foreach (Actor actor in gems)
+            {
+                //Making artifacts move
+                Artifact artifact = (Artifact) actor;
+                artifact.MoveNext(maxX, maxY);
+
+                //SET CONDITION HERE
+                if (robot.GetPosition().Equals(actor.GetPosition()))
+                {
+                    robot.SetScore(1);
+                    banner.SetText("Score: " + robot.GetScore().ToString());
+                }
+            } 
+        }
+
+        /// <summary>
+        /// Draws the actors on the screen.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        public void DoOutputs(Cast cast)
+        {
+            List<Actor> actors = cast.GetAllActors();
+            _videoService.ClearBuffer();
+            _videoService.DrawActors(actors);
+            _videoService.FlushBuffer();
+        }
+
+    }
+}
